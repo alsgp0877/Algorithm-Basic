db8b84d3ffde0246e1d7a722ba951d7a7c59412f
alsgp0877 <alsgp0877@naver.com>
2022-04-09 17:04:33 +0900

    남은일수구하기 2차원배열은 정말 유용할것 같다

---
+++ src/기본자료구조/실습8.java
@@ -0,0 +1,22 @@
+package 기본자료구조;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class 실습8 {
+		public static void main(String[] args) {
+			double[] a = {1.0,2.0,3.0,4.0,5.0};
+			
+			for(int i=0;i<a.length;i++) {
+				System.out.println("a["+i+"]="+a[i]);
+			}
+			double sum =0;
+			for(double i : a) {
+				sum+=i;
+			}
+			
+			System.out.println("모든요소의 합은"+sum+"입니다.");
+		
+		}
+
+}
